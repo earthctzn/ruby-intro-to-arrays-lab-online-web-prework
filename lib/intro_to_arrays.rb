50e492f83fb86232bc84cccb4428dc5300c6b359
Caleb Einarr <earthctzn1@gmail.com>
2019-04-05 16:05:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,7 +8,7 @@ def array_with_two_elements
   return @my_two_array
 end
 
-def first_element(@taylor_swift)
+def first_element(taylor_swift)
   @taylor_swift = Array.new("Welcome to New York", "Blank Space", "Style", "Out of The Woods")
   my_first_element = first_element(@taylor_swift)
   puts my_first_element
